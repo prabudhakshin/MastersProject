6dc8600eb3f6aac9dbc37951cd839215e3e06bf9
Prabu Dhakshinamurthy <pdhakshi@ccied1.sysnet.ucsd.edu>
2013-02-15 19:20:05 -0800

    udfs

--- script/UDFS/myudfs/FindBucket.java
+++ script/UDFS/myudfs/FindBucket.java
@@ -29,21 +29,16 @@ public class FindBucket extends EvalFunc<String>
       String[] day = inputSplitPath.split("\\.");
       inputSplit = day[1];
 
-      int queryContrib = 0;
       // Identify query type bucket
       String queryTypeBucket = "";
       if (queryType == 1 /*A record; About 66% */) {
         queryTypeBucket = "A";
-        queryContrib = 66;
       } else if (queryType == 12 /*PTR record; About 15% */) {
         queryTypeBucket = "PTR";
-        queryContrib = 15;
        } else if (queryType == 28 /*AAAA record; About 12% */) {
         queryTypeBucket = "AAAA";
-        queryContrib = 12;
       } else /*others; 7%*/ {
         queryTypeBucket = "OTHR";
-        queryContrib = 7;
       }
 
       // Identify TLD bucket
@@ -54,24 +49,62 @@ public class FindBucket extends EvalFunc<String>
       String[] domain_parts = registeredDomain.split("\\.");
       String tld = domain_parts[(domain_parts.length)-1];
 
-      int tldContrib = 0;
       String topLevelDomain = "";
       if (tld.equals("com") || tld.equals("COM")) /*44% */ {
         topLevelDomain = "COM";
-        tldContrib = 44;
       } else if (tld.equals("net") || tld.equals("NET") /*26*/) {
         topLevelDomain = "NET";
-        tldContrib = 26;
       } else if (tld.equals("arpa") || tld.equals("ARPA") /*15*/) {
         topLevelDomain = "ARPA";
-        tldContrib = 15;
       } else /*15%*/ {
         topLevelDomain = "OTHR";
-        tldContrib = 15;
+      }
+
+      String qtype_tld = queryTypeBucket + "_" + topLevelDomain;
+      double percent_of_buckets_for_current_group  = 0;
+
+      if (qtype_tld.equals("A_COM")) {
+        percent_of_buckets_for_current_group = 34.418;
+      } else if (qtype_tld.equals("A_NET")) {
+        percent_of_buckets_for_current_group = 21.807;
+      } else if (qtype_tld.equals("A_ARPA")) {
+        percent_of_buckets_for_current_group = 0.129;
+      } else if (qtype_tld.equals("A_OTHR")) {
+        percent_of_buckets_for_current_group = 9.910;
+      }
+
+      else if (qtype_tld.equals("PTR_COM")) {
+        percent_of_buckets_for_current_group = 0.108;
+      } else if (qtype_tld.equals("PTR_NET")) {
+        percent_of_buckets_for_current_group = 0.063;
+      } else if (qtype_tld.equals("PTR_ARPA")) {
+        percent_of_buckets_for_current_group = 14.869;
+      } else if (qtype_tld.equals("PTR_OTHR")) {
+        percent_of_buckets_for_current_group = 0.029;
+      }
+
+      else if (qtype_tld.equals("AAAA_COM")) {
+        percent_of_buckets_for_current_group = 7.225;
+      } else if (qtype_tld.equals("AAAA_NET")) {
+        percent_of_buckets_for_current_group = 3.468;
+      } else if (qtype_tld.equals("AAAA_ARPA")) {
+        percent_of_buckets_for_current_group = 0.004;
+      } else if (qtype_tld.equals("AAAA_OTHR")) {
+        percent_of_buckets_for_current_group = 2.407;
+      }
+
+      else if (qtype_tld.equals("OTHR_COM")) {
+        percent_of_buckets_for_current_group = 2.703;
+      } else if (qtype_tld.equals("OTHR_NET")) {
+        percent_of_buckets_for_current_group = 1.390;
+      } else if (qtype_tld.equals("OTHR_ARPA")) {
+        percent_of_buckets_for_current_group = 0.306;
+      } else if (qtype_tld.equals("OTHR_OTHR")) {
+        percent_of_buckets_for_current_group = 1.163;
       }
 
       // Identify domain bucket based on hashcode value
-      long numbuckets = (long) (TOTBUCKETS* (queryContrib*tldContrib/10000.0));
+      long numbuckets = (long) (TOTBUCKETS*percent_of_buckets_for_current_group/100.0);
       if (numbuckets == 0)
         numbuckets = 1;
 
@@ -80,13 +113,11 @@ public class FindBucket extends EvalFunc<String>
       long bucket_id = unsignedHashCode % numbuckets;
       String domainBucketNumber = String.valueOf(bucket_id);
 
+      // System.out.println("Total buckets: " + numbuckets + "; Current bucket: " + domainBucketNumber);
+
       // return string of type
       // '20120201_AAAA_NET_198'
-      // return (inputSplit + "_" + queryTypeBucket + "_" + 
-      //        topLevelDomain + "_" + domainBucketNumber);
-      //return (inputSplit + "_" + "A" + "_" + 
-      //        "COM" + "_" + domainBucketNumber);
-
-      return (queryTypeBucket + "_" + topLevelDomain);
+       return (inputSplit + "_" + queryTypeBucket + "_" + 
+              topLevelDomain + "_" + domainBucketNumber);
     }
 }
