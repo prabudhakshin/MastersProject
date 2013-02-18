cea91c0e95d7584d0a70e69fe1c9d438cc69e4e3
Prabu Dhakshinamurthy <pdhakshi@ccied1.sysnet.ucsd.edu>
2013-02-17 21:32:48 -0800

    revdomain change

--- script/UDFS/compile.sh
+++ script/UDFS/compile.sh
@@ -4,5 +4,7 @@ javac -cp ../pig.jar ParseDayFromFileName.java
 javac -cp ../pig.jar MyUDF.java
 javac -cp ../pig.jar BucketizeByHashCode.java
 javac -cp ../pig.jar ExtractTLD.java
+javac -cp ../pig.jar FindBucket.java
+javac -cp ../pig.jar Dedup.java
 cd ../
 jar -cf myudfs.jar myudfs
