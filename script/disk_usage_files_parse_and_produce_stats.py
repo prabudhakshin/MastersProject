9bf016b1fbc0cb575c695b530925b694d8f41fdf
Prabu D <prabudhakshin@gmail.com>
2013-02-26 12:09:44 -0800

    reorg

--- script/disk_usage_files_parse_and_produce_stats.py
+++ script/disk_usage_files_parse_and_produce_stats.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 import sys
 import re
 import numpy
@@ -14,15 +14,16 @@ for line in fileHandle:
   fileSize = float(filesizeCol[:-1])
   if (filesizeCol[-1] == 'g'):
     fileSize = fileSize * 1024
-  else if (filesizeCol[-1] == 'm'):
+  elif (filesizeCol[-1] == 'm'):
     fileSize = fileSize
-  else if (filesizeCol[-1] == 'k'):
+  elif (filesizeCol[-1] == 'k'):
     fileSize = fileSize / 1024.0
   else:
     print ("Weird file size: {}",format(filesizeCol))
     pass
   fileSizeMBytes.append(fileSize)
 
-print ("{:20>} | {}".format("Total Entries:"), len(fileSizeMBytes))
-print ("{:20>} | {:6.2f>}".format("Stand. Dev:"), numpy.std(fileSizeMBytes))
-print ("{:20>} | {:6.2f>}".format("Mean:"), numpy.mean(fileSizeMBytes))
+print ("{:>20} | {:>6.2f}".format("Total Entries:", len(fileSizeMBytes)))
+print ("{:>20} | {:>6.2f}".format("TotalSize in MB:", sum(fileSizeMBytes)))
+print ("{:>20} | {:>6.2f}".format("Stand. Dev:", numpy.std(fileSizeMBytes)))
+print ("{:>20} | {:>6.2f}".format("Mean:", numpy.mean(fileSizeMBytes)))
